b738a7fb295076feb65b6dafc9ff40488f70fd38
paoddg <paoladanieladg3@gmail.com>
2022-11-10 22:21:42 -0500

    .

--- lab9/exc2script.js
+++ lab9/exc2script.js
@@ -1,8 +1,4 @@
-
 var todayFullDate = new Date();
-
 var todayFullYear = todayFullDate.getFullYear();
-
 var age = prompt("What is your age?");
-
 document.getElementById("output").innerHTML = todayFullYear - age;
