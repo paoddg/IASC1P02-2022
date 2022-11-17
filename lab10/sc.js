29fdf57143f454e7c939cb71d04e9bd7d87bba5a
paoddg <paoladanieladg3@gmail.com>
2022-11-16 21:11:56 -0500

    .

--- lab10/sc.js
+++ lab10/sc.js
@@ -1,23 +1,17 @@
-
-
 var elementOne = document.getElementById("firstNumber");
 var elementTwo = document.getElementById("secondNumber");
-
 function sum() {
   var calculation = Number(elementOne.value) + Number(elementTwo.value);
   document.getElementById("Answer").innerHTML = calculation;
 }
-
 function difference() {
   var calculation = Number(elementOne.value) - Number(elementTwo.value);
   document.getElementById("Answer").innerHTML = calculation;
 }
-
 function product() {
   var calculation = Number(elementOne.value) * Number(elementTwo.value);
   document.getElementById("Answer").innerHTML = calculation;
 }
-
 function quotient() {
   var calculation = Number(elementOne.value) / Number(elementTwo.value);
   document.getElementById("Answer").innerHTML = calculation;
