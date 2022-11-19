9450a91f139b97a48bb91bed7077e336499d44c4
paoddg <paoladanieladg3@gmail.com>
2022-11-19 11:05:48 -0500

    .

--- lab10/sc.js
+++ lab10/sc.js
@@ -1,17 +1,21 @@
 var elementOne = document.getElementById("firstNumber");
 var elementTwo = document.getElementById("secondNumber");
+
 function sum() {
   var calculation = Number(elementOne.value) + Number(elementTwo.value);
   document.getElementById("Answer").innerHTML = calculation;
 }
+
 function difference() {
   var calculation = Number(elementOne.value) - Number(elementTwo.value);
   document.getElementById("Answer").innerHTML = calculation;
 }
+
 function product() {
   var calculation = Number(elementOne.value) * Number(elementTwo.value);
   document.getElementById("Answer").innerHTML = calculation;
 }
+
 function quotient() {
   var calculation = Number(elementOne.value) / Number(elementTwo.value);
   document.getElementById("Answer").innerHTML = calculation;
