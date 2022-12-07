d2230d674bd40983461c8e2d5902241e6d7c45da
paoddg <paoladanieladg3@gmail.com>
2022-12-06 22:04:48 -0500

    .

--- Assignments/Assignment4/main.js
+++ Assignments/Assignment4/main.js
@@ -1,2 +1,49 @@
-var dt = new Date();
-document.getElementById("datetime").innerHTML = (("0"+dt.getDate()).slice(-2)) +"."+ (("0"+(dt.getMonth()+1)).slice(-2)) +"."+ (dt.getFullYear()) +" "+ (("0"+dt.getHours()).slice(-2)) +":"+ (("0"+dt.getMinutes()).slice(-2));
+//The following code was done using a tutorial at https://www.elated.com/creating-a-javascript-clock/
+function updateClock() {
+  var currentTime = new Date();
+
+  var currentHours = currentTime.getHours();
+  var currentMinutes = currentTime.getMinutes();
+  var currentSeconds = currentTime.getSeconds();
+
+  // Pad the minutes and seconds with leading zeros, if required
+  var minutePrefix;
+  if (currentMinutes < 10) {
+    minutePrefix = "0";
+  } else {
+    minutePrefix = "";
+  }
+  currentMinutes = minutePrefix + currentMinutes;
+
+  var secondPrefix;
+  if (currentSeconds < 10) {
+    secondPrefix = "0";
+  } else {
+    secondPrefix = "";
+  }
+  currentSeconds = secondPrefix + currentSeconds;
+
+  // Choose either "AM" or "PM" as appropriate
+  var timeOfDay = currentHours < 12 ? "AM" : "PM";
+
+  // Convert the hours component to 12-hour format if needed
+  currentHours = currentHours > 12 ? currentHours - 12 : currentHours;
+
+  // Convert an hours component of "0" to "12"
+  currentHours = currentHours == 0 ? 12 : currentHours;
+
+  // Compose the string for display
+  var currentTimeString =
+    currentHours +
+    ":" +
+    currentMinutes +
+    ":" +
+    currentSeconds +
+    " " +
+    timeOfDay;
+
+  // Update the time display
+  document.getElementById("Clock").innerHTML = currentTimeString;
+}
+updateClock();
+window.setInterval(updateClock, 1000);
