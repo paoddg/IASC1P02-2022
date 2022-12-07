913340aff44ad30e8de34551ca2e3ebee24a4715
paoddg <paoladanieladg3@gmail.com>
2022-12-06 22:44:52 -0500

    .

--- Assignments/Assignment4/main.js
+++ Assignments/Assignment4/main.js
@@ -1,16 +1,7 @@
 //Found on https://www.programiz.com/javascript/examples/display-current-date
-// program to display the date
-// get local machine date time
-const date = new Date();
-
-// get the date as a string
-const n = date.toDateString();
-
-// get the time as a string
-const time = date.toLocaleTimeString();
-
-// display date
-console.log('Date: ' + n);
-
-// display time
-console.log('Time: ' + time);
+function display_ct() {
+var x = new Date()
+var x1=x.toUTCString();// changing the display to UTC string
+document.getElementById('Clock').innerHTML = x1;
+tt=display_c();
+ }
