defa12f2ea1b4f3dad09dcac3477e69e5e66ece3
paoddg <paoladanieladg3@gmail.com>
2022-12-06 22:45:25 -0500

    .

--- Assignments/Assignment4/main.js
+++ Assignments/Assignment4/main.js
@@ -1,5 +1,5 @@
 //Found on https://www.programiz.com/javascript/examples/display-current-date
-function display_ct() {
+function display_Clock() {
 var x = new Date()
 var x1=x.toUTCString();// changing the display to UTC string
 document.getElementById('Clock').innerHTML = x1;
