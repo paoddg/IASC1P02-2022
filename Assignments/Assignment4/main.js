33307e33091832b044bc575cca488dcb352d00ad
paoddg <paoladanieladg3@gmail.com>
2022-12-06 22:58:51 -0500

    .

--- Assignments/Assignment4/main.js
+++ Assignments/Assignment4/main.js
@@ -1,8 +1,3 @@
 //Found on https://www.programiz.com/javascript/examples/display-current-date
-
-var today = new Date();
-var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-var dateTime = date+' '+time;
-
-console.log(dateTime)
+var dt = new Date();
+document.getElementById('date-time').innerHTML=dt;
