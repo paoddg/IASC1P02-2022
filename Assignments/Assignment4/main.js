ff06ea1e8a35bdc12b68067b3e46c96bd41ddd96
paoddg <paoladanieladg3@gmail.com>
2022-12-07 00:53:30 -0500

    .

--- Assignments/Assignment4/main.js
+++ Assignments/Assignment4/main.js
@@ -1,3 +1,38 @@
+var school = document.getElementById("school");
+var interests = document.getElementById("interests");
+var goals = document.getElementById("goals");
+
+var schoolButton = document.getElementById("schoolButton");
+var interestsButton = document.getElementById("interestsButton");
+var goalsButton = document.getElementById("goalsButton");
+
+function switchOffEverything() {
+  school.style.display = "none";
+  interests.style.display = "none";
+  goals.style.display = "none";
+}
+
+function switchToSchool() {
+  switchOffEverything();
+  school.style.display = "block";
+}
+
+schoolButton.onclick = switchToSchool;
+
+function switchToInterests() {
+  switchOffEverything();
+  interests.style.display = "block";
+}
+
+interestsButton.onclick = switchToInterests;
+
+function switchToGoals() {
+  switchOffEverything();
+  goals.style.display = "block";
+}
+
+goalsButton.onclick = switchToGoals;
+
 //Found on https://www.howtocodeschool.com/2020/05/display-current-date-and-time-using-html-javascript.html
 var dt = new Date();
 document.getElementById('date-time').innerHTML=dt;
